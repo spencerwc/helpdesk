2b4181c8ef64a235aa768539522b787ec9472699
Spencer Coleman <spencer@swc.dev>
2023-08-20 23:42:04 -0700

    Scaffold AuthForm to handle signup and login

---
+++ app/(auth)/AuthForm.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useState } from 'react';
+
+interface AuthFormProps {
+    handleSubmit: (
+        event: React.FormEvent<HTMLFormElement>,
+        email: string,
+        password: string
+    ) => void;
+}
+
+export default function AuthForm({ handleSubmit }: AuthFormProps) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <form onSubmit={(e) => handleSubmit(e, email, password)}>
+            <label>
+                <span>Email:</span>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+        </form>
+    );
+}
