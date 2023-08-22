3b24205f70856588228c9ae0135f4f0e874a4966
Spencer Coleman <spencer@swc.dev>
2023-08-21 19:58:36 -0700

    Handle getting and displaying current session user

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,25 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 import Navbar from '../components/Navbar';
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const supabase = createServerComponentClient({ cookies });
+    const { data } = await supabase.auth.getSession();
+    const user = data.session?.user;
+
     return (
         <>
             <Navbar />
             {children}
+            {user && (
+                <footer className="text-center text-sm">
+                    Logged in as {user.email}
+                </footer>
+            )}
         </>
     );
 }
