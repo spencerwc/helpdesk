0c289b687ded0b6c8ab8e46e32bb3a4fe7800b57
Spencer Coleman <spencer@swc.dev>
2023-08-22 19:52:13 -0700

    Handle logging user in with supabase on login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import AuthForm from '../AuthForm';
+
 export default function Login() {
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    const handleSubmit = async (
+        event: React.FormEvent,
+        email: string,
+        password: string
+    ) => {
+        event.preventDefault();
+        setError('');
+
+        const supabase = createClientComponentClient();
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) {
+            setError(error.message);
+        } else {
+            router.push('/');
+        }
+    };
+
     return (
         <main>
             <h2 className="text-center">Log in</h2>
+            <AuthForm handleSubmit={handleSubmit} />
+            {error && <div className="error">{error}</div>}
         </main>
     );
 }
