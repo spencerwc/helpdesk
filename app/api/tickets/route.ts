ed177bd7b1a0dad2e6abc316a31c1ce86676b884
Spencer Coleman <spencer@swc.dev>
2023-08-23 23:43:25 -0700

    Set up api route for creating tickets

---
+++ app/api/tickets/route.ts
@@ -0,0 +1,35 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse, type NextRequest } from 'next/server';
+import { cookies } from 'next/headers';
+
+export const dynamic = 'force-dynamic';
+
+interface Ticket {
+    body: string;
+    createdAt?: string;
+    id?: string;
+    priority: string;
+    title: string;
+    user_email?: string;
+}
+
+export async function POST(request: NextRequest) {
+    const ticket: Ticket = await request.json();
+
+    const supabase = createRouteHandlerClient({ cookies });
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    const { data, error } = await supabase
+        .from('tickets')
+        .insert({
+            ...ticket,
+            user_email: session?.user.email,
+        })
+        .select()
+        .single();
+
+    return NextResponse.json({ data, error });
+}
